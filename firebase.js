8f0662cdb609b7b67878549144bba73a9173f451
tedtheotheodoro <tedtheotheodoro@gmail.com>
2025-03-27 09:53:36 -0300

    chore: restaura configuração Firebase.js

--- firebase.js
+++ firebase.js
@@ -1,2 +1,14 @@
 
-// Placeholder para Firebase
+// firebase.js
+// 🔥 Substitua as credenciais abaixo pelas do seu projeto Firebase Console
+const firebaseConfig = {
+    apiKey: "AIzaSyBd_EsQp_CLn5DCUUa9a8FhSFFXcHZIayI",
+    authDomain: "oscar-simulator.firebaseapp.com",
+    projectId: "oscar-simulator",
+    storageBucket: "oscar-simulator.firebasestorage.app",
+    messagingSenderId: "966184712255",
+    appId: "1:966184712255:web:d1bec12a4cbd96df13e08a"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
