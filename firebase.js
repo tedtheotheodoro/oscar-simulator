86b1db3046e51a598ce54e8565fda534dc6aa1ff
tedtheotheodoro <tedtheotheodoro@gmail.com>
2025-03-27 08:07:57 -0300

    chore: atualiza config do Firebase

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 
 // Firebase config - substitua com suas credenciais
 const firebaseConfig = {
-    apiKey: "AIzaSyBd_EsQp_CLn5DCUUa9a8FhSFFXcHZIayI",
-    authDomain: "oscar-simulator.firebaseapp.com",
-    projectId: "oscar-simulator",
-    storageBucket: "oscar-simulator.firebasestorage.app",
-    messagingSenderId: "966184712255",
-    appId: "1:966184712255:web:d1bec12a4cbd96df13e08a"
-  };
+  apiKey: "AIzaSyBd_EsQp_CLn5DCUUa9a8FhSFFXcHZIayI",
+  authDomain: "oscar-simulator.firebaseapp.com",
+  projectId: "oscar-simulator",
+  storageBucket: "oscar-simulator.firebasestorage.app",
+  messagingSenderId: "966184712255",
+  appId: "1:966184712255:web:d1bec12a4cbd96df13e08a"
+};
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
